65ecb286246fd32ce9d6140897c4debcacda11af
RealLumala <dlumala20@gmail.com>
2026-02-16 15:10:02 +0300

    Ignition

---
+++ programs/mizly/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("APQtcvd4R68x6a8DcLCjyQWryyLn3UcoSEmuPUsbj3B9");
+
+#[program]
+pub mod mizly {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
